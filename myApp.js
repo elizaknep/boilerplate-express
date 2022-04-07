2974dacab5ea0c610759480c20780714864f6f03
Beth Knepper <elizaknep@gmail.com>
2022-04-07 12:09:47 -0500

    Add /public route with middleware

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ var app = express();
 console.log('Hello world');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-})
+  res.sendFile(`${__dirname}/views/index.html`);
+});
 
+app.use('/public', express.static(`${__dirname}/public`));
 
 
 
